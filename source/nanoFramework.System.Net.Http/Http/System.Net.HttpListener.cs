7542510548470ccf9c0611c0d59c724ccbd7abcb
mklatecki <mateusz.klatecki@gc5.pl>
2020-04-24 09:24:02 +0200

    maybe preincrementation

--- source/nanoFramework.System.Net.Http/Http/System.Net.HttpListener.cs
+++ source/nanoFramework.System.Net.Http/Http/System.Net.HttpListener.cs
@@ -367,7 +367,7 @@ namespace System.Net
                 }
                 catch (SocketException)
                 {
-                    if (retry++ > 5)
+                    if (++retry > 5)
                     {
                         // If request to stop listener flag is set or locking call is interupted return
                         // On exception we stop the service and record the exception.
