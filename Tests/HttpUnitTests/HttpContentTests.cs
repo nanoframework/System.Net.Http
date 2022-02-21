3b741b766198f712b80d58f8eb31e9a3924cfac4
josesimoes <jose.simoes@eclo.solutions>
2022-02-21 11:19:44 +0000

    Add HttContent tests

---
+++ Tests/HttpUnitTests/HttpContentTests.cs
@@ -0,0 +1,121 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using nanoFramework.TestFramework;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+
+namespace HttpUnitTests
+{
+    [TestClass]
+    public class HttpContentTest
+    {
+        [TestMethod]
+        public void Dispose_BufferContentThenDisposeContent_BufferedStreamGetsDisposed()
+        {
+            MockContent content = new MockContent();
+            content.LoadIntoBuffer();
+
+            Type type = typeof(HttpContent);
+            
+            FieldInfo bufferedContentField = type.GetField("_buffer", BindingFlags.Instance | BindingFlags.NonPublic);
+            
+            Assert.NotNull(bufferedContentField, "_buffer field shouldn't be null");
+
+            MemoryStream bufferedContentStream = bufferedContentField.GetValue(content) as MemoryStream;
+            Assert.NotNull(bufferedContentStream, "bufferedContentStream field shouldn't be null");
+
+            content.Dispose();
+
+            // The following line will throw an ObjectDisposedException if the buffered-stream was correctly disposed.
+            Assert.Throws(typeof(ObjectDisposedException),
+                () => 
+                { 
+                    string str = bufferedContentStream.Length.ToString();
+                });
+        }
+
+        public void LoadIntoBuffer_ContentLengthSmallerThanActualData_ActualDataLargerThanMaxSize_ThrowsException()
+        {
+            BufferTestConfig[] bufferTests = new BufferTestConfig[]
+            {
+                new BufferTestConfig(1, 100, 99, 1),
+                new BufferTestConfig(1, 100, 50, 99),
+                new BufferTestConfig(1, 100, 98, 98),
+                new BufferTestConfig(1, 100, 99, 99),
+                new BufferTestConfig(1, 100, 99, 98),
+                new BufferTestConfig(3, 50, 100, 149),
+                new BufferTestConfig(3, 50, 149, 149)
+            };
+
+            foreach (var testConfig in bufferTests)
+            {
+                Assert.True((testConfig.MaxSize >= 1 && testConfig.MaxSize <= (testConfig.NumberOfWrites * testConfig.SizeOfEachWrite) - 1), "Config values out of range.");
+
+                Assert.Throws(typeof(HttpRequestException),
+                    () =>
+                    {
+                        LieAboutLengthContent c = new(
+                            testConfig.NumberOfWrites,
+                            testConfig.SizeOfEachWrite,
+                            testConfig.ReportedLength);
+
+                        c.LoadIntoBuffer();
+                    });
+            }
+        }
+
+        private class BufferTestConfig
+        {
+            public int NumberOfWrites { get; set; }
+            public int SizeOfEachWrite { get; set; }
+            public int ReportedLength { get; set; }
+            public int MaxSize { get; set; }
+
+            public BufferTestConfig(
+                int numberOfWrites,
+                int sizeOfEachWrite,
+                int reportedLength,
+                int maxSize1)
+            {
+                NumberOfWrites = numberOfWrites;
+                SizeOfEachWrite = sizeOfEachWrite;
+                ReportedLength = reportedLength;
+                MaxSize = maxSize1;
+            }
+        }
+
+        private sealed class LieAboutLengthContent : HttpContent
+        {
+            private readonly int _numberOfWrites, _sizeOfEachWrite, _reportedLength;
+
+            public LieAboutLengthContent(int numberOfWrites, int sizeOfEachWrite, int reportedLength)
+            {
+                _numberOfWrites = numberOfWrites;
+                _sizeOfEachWrite = sizeOfEachWrite;
+                _reportedLength = reportedLength;
+            }
+
+            protected override void SerializeToStream(Stream stream)
+            {
+                byte[] bytes = new byte[_sizeOfEachWrite];
+
+                for (int i = 0; i < _numberOfWrites; i++)
+                {
+                   stream.Write(bytes, 0, bytes.Length);
+                }
+            }
+
+            protected override bool TryComputeLength(out long length)
+            {
+                length = _reportedLength;
+                return true;
+            }
+        }
+    }
+}
---
+++ Tests/HttpUnitTests/MockContent.cs
@@ -0,0 +1,172 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace HttpUnitTests
+{
+    [Flags]
+    public enum MockOptions
+    {
+        None = 0x0,
+        ThrowInSerializeMethods = 0x1,
+        ReturnNullInCopyToAsync = 0x2,
+        UseWriteByteInCopyTo = 0x4,
+        DontOverrideCreateContentReadStream = 0x8,
+        CanCalculateLength = 0x10,
+        ThrowInTryComputeLength = 0x20,
+        ThrowInAsyncSerializeMethods = 0x40
+    }
+
+    public class MockException : Exception
+    {
+        public MockException() { }
+        public MockException(string message) : base(message) { }
+        public MockException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    public class MockContent : HttpContent
+    {
+        private byte[] _mockData;
+        private MockOptions _options;
+        private Exception _customException;
+
+        public int TryComputeLengthCount { get; private set; }
+        public int SerializeToStreamAsyncCount { get; private set; }
+        public int CreateContentReadStreamCount { get; private set; }
+        public int DisposeCount { get; private set; }
+
+        public byte[] MockData
+        {
+            get { return _mockData; }
+        }
+
+        public MockContent()
+            : this((byte[])null, MockOptions.None)
+        {
+        }
+
+        public MockContent(byte[] mockData)
+            : this(mockData, MockOptions.None)
+        {
+        }
+
+        public MockContent(MockOptions options)
+            : this((byte[])null, options)
+        {
+        }
+
+        public MockContent(Exception customException, MockOptions options)
+            : this((byte[])null, options)
+        {
+            _customException = customException;
+        }
+
+        public MockContent(byte[] mockData, MockOptions options)
+        {
+            _options = options;
+
+            if (mockData == null)
+            {
+                _mockData = Encoding.UTF8.GetBytes("data");
+            }
+            else
+            {
+                _mockData = mockData;
+            }
+        }
+
+        public byte[] GetMockData()
+        {
+            return _mockData;
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            TryComputeLengthCount++;
+
+            if ((_options & MockOptions.ThrowInTryComputeLength) != 0)
+            {
+                throw new MockException();
+            }
+
+            if ((_options & MockOptions.CanCalculateLength) != 0)
+            {
+                length = _mockData.Length;
+                return true;
+            }
+            else
+            {
+                length = 0;
+                return false;
+            }
+        }
+
+        protected override void SerializeToStream(Stream stream)
+        {
+            SerializeToStreamAsyncCount++;
+
+            if ((_options & MockOptions.ReturnNullInCopyToAsync) != 0)
+            {
+                return;
+            }
+
+            if ((_options & MockOptions.ThrowInAsyncSerializeMethods) != 0)
+            {
+                throw _customException;
+            }
+
+            stream.Write(_mockData, 0, _mockData.Length);
+        }
+
+        //protected override Stream CreateContentReadStream()
+        //{
+        //    CreateContentReadStreamCount++;
+
+        //    if ((_options & MockOptions.DontOverrideCreateContentReadStream) != 0)
+        //    {
+        //        return base.CreateContentReadStream();
+        //    }
+        //    else
+        //    {
+        //        return new MockMemoryStream(_mockData);
+        //    }
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            DisposeCount++;
+            base.Dispose(disposing);
+        }
+
+        private void CheckThrow()
+        {
+            if ((_options & MockOptions.ThrowInSerializeMethods) != 0)
+            {
+                throw _customException;
+            }
+        }
+    }
+
+    public class MockMemoryStream : MemoryStream
+    {
+        public int DisposeCount { get; private set; }
+
+        public MockMemoryStream(byte[] buffer)
+            : base(buffer)
+        {
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            DisposeCount++;
+            base.Dispose(disposing);
+        }
+    }
+}
