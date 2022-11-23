89b758478eef37ad4cbb5e18dce4089e9e44fd74
José Simões <jose.simoes@eclo.solutions>
2022-11-23 01:14:36 +0000

    Work in HttpUtility class (#370)
    ***NO_CI***

--- nanoFramework.System.Net.Http/Http/System.Net.HttpUtility.cs
+++ nanoFramework.System.Net.Http/Http/System.Net.HttpUtility.cs
@@ -4,21 +4,20 @@
 // See LICENSE file in the project root for full license information.
 //
 
-using System;
 using System.Text;
 
 namespace System.Web
 {
     /// <summary>
-    /// Utilities to encode and decode url
+    /// Utilities to encode and decode URLs.
     /// </summary>
     public class HttpUtility
     {
         /// <summary>
-        /// Encode an URL using UTF8
+        /// Encodes a URL string.
         /// </summary>
-        /// <param name="str">The URL string to encode</param>
-        /// <returns>The encoded string URL</returns>
+        /// <param name="str">The text to encode.</param>
+        /// <returns>An encoded string.</returns>
         public static string UrlEncode(string str)
         {
             if ((str == null) || (str == string.Empty))
@@ -30,12 +29,14 @@ namespace System.Web
         }
 
         /// <summary>
-        /// Encode an URL
+        /// Encodes a URL string using the specified encoding object.
         /// </summary>
-        /// <param name="str">The URL string to encode</param>
-        /// <param name="e">The Encoding object that specifies the encoding scheme</param>
-        /// <returns>The encoded string URL</returns>
-        public static string UrlEncode(string str, Encoding e)
+        /// <param name="str">The text to encode.</param>
+        /// <param name="e">The <see cref="Encoding"/> object that specifies the encoding scheme.</param>
+        /// <returns>An encoded string.</returns>
+        public static string UrlEncode(
+            string str,
+            Encoding e)
         {
             if ((str == null) || (str == string.Empty))
             {
@@ -46,63 +47,81 @@ namespace System.Web
         }
 
         /// <summary>
-        /// Encode an URL
+        /// Converts a byte array into a URL-encoded string, starting at the specified position in the array and continuing for the specified number of bytes.
         /// </summary>
-        /// <param name="bytes">The array of bytes to encode</param>
-        /// <param name="offset">The position in the byte array at which to begin encoding</param>
-        /// <param name="count">The number of bytes to encode</param>
-        /// <returns>The encoded string URL</returns>
-        public static string UrlEncode(byte[] bytes, int offset, int count)
+        /// <param name="bytes">The array of bytes to encode.</param>
+        /// <param name="offset">The position in the byte array at which to begin encoding.</param>
+        /// <param name="count">The number of bytes to encode.</param>
+        /// <returns>An encoded string.</returns>
+        public static string UrlEncode(
+            byte[] bytes,
+            int offset,
+            int count)
         {
-            return new string(Encoding.UTF8.GetChars(UrlEncodeBytesToBytesInternal(bytes, 0, bytes.Length, false)));
+            return new string(Encoding.UTF8.GetChars(UrlEncodeBytesToBytesInternal(bytes, offset, count, false)));
         }
 
         /// <summary>
-        /// Encode an URL
+        /// Converts a byte array into an encoded URL string.
         /// </summary>
-        /// <param name="bytes">The array of bytes to encode</param>
-        /// <returns>The encoded string URLL</returns>
+        /// <param name="bytes">The array of bytes to encode.</param>
+        /// <returns>An encoded string.</returns>
         public static string UrlEncode(byte[] bytes) => UrlEncode(bytes, 0, bytes.Length);
 
         /// <summary>
-        /// Encode an URL into a byte array
+        /// Converts a string into a URL-encoded array of bytes using the specified encoding object.
         /// </summary>
-        /// <param name="str">The URL string to encode</param>
-        /// <param name="e"></param>
-        /// <returns>The encoded byte array</returns>
+        /// <param name="str">The string to encode.</param>
+        /// <param name="e">The <see cref="Encoding"/> that specifies the encoding scheme.</param>
+        /// <returns>An encoded array of bytes.</returns>
         public static byte[] UrlEncodeToBytes(string str, Encoding e)
         {
             if (str == null)
             {
                 return null;
             }
+
             var bytes = e.GetBytes(str);
+            
             return UrlEncodeBytesToBytesInternal(bytes, 0, bytes.Length, false);
         }
 
         /// <summary>
-        /// Encode a byte array URL into a byte array
+        /// Converts a URL-encoded array of bytes into a decoded array of bytes.
         /// </summary>
         /// <param name="bytes">The array of bytes to encode</param>
-        /// <returns>The encoded byte array</returns>
+        /// <returns>A decoded array of bytes.</returns>
         public static byte[] UrlEncodeToBytes(byte[] bytes) => UrlEncodeBytesToBytesInternal(bytes, 0, bytes.Length, false);
 
         /// <summary>
-        /// Encode a byte array URL into a byte array
+        /// Converts an array of bytes into a URL-encoded array of bytes, starting at the specified position in the array and continuing for the specified number of bytes.
         /// </summary>
         /// <param name="bytes">The array of bytes to encode</param>
         /// <param name="offset">The position in the byte array at which to begin encoding</param>
         /// <param name="count">The number of bytes to encode</param>
-        /// <returns>The encoded byte array</returns>		
-        public static byte[] UrlEncodeToBytes(byte[] bytes, int offset, int count) => UrlEncodeBytesToBytesInternal(bytes, offset, count, false);
+        /// <returns>An encoded array of bytes.</returns>		
+        public static byte[] UrlEncodeToBytes(
+            byte[] bytes,
+            int offset,
+            int count) => UrlEncodeBytesToBytesInternal(
+                bytes,
+                offset,
+                count,
+                false);
 
-        private static byte[] UrlEncodeBytesToBytesInternal(byte[] bytes, int offset, int count, bool alwaysCreateReturnValue)
+        private static byte[] UrlEncodeBytesToBytesInternal(
+            byte[] bytes,
+            int offset,
+            int count,
+            bool alwaysCreateReturnValue)
         {
             var num = 0;
             var num2 = 0;
+            
             for (var i = 0; i < count; i++)
             {
                 var ch = (char)bytes[offset + i];
+            
                 if (ch == ' ')
                 {
                     num++;
@@ -112,17 +131,20 @@ namespace System.Web
                     num2++;
                 }
             }
-            if ((!alwaysCreateReturnValue && (num == 0)) &&
-                (num2 == 0))
+            if ((!alwaysCreateReturnValue && (num == 0))
+                && (num2 == 0))
             {
                 return bytes;
             }
+
             var buffer = new byte[count + (num2 * 2)];
             var num4 = 0;
+            
             for (var j = 0; j < count; j++)
             {
                 var num6 = bytes[offset + j];
                 var ch2 = (char)num6;
+                
                 if (IsSafe(ch2))
                 {
                     buffer[num4++] = num6;
@@ -138,6 +160,7 @@ namespace System.Web
                     buffer[num4++] = (byte)IntToHex(num6 & 15);
                 }
             }
+
             return buffer;
         }
 
@@ -153,11 +176,12 @@ namespace System.Web
 
         private static bool IsSafe(char ch)
         {
-            if ((((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z'))) ||
-                ((ch >= '0') && (ch <= '9')))
+            if ((((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z')))
+                || ((ch >= '0') && (ch <= '9')))
             {
                 return true;
             }
+
             switch (ch)
             {
                 case '(':
@@ -173,27 +197,28 @@ namespace System.Web
         }
 
         /// <summary>
-        /// Decode an UTF8 encoded URL
+        /// Converts a string that has been encoded for transmission in a URL into a decoded string.
         /// </summary>
-        /// <param name="url">The encoded URL</param>
+        /// <param name="str">The string to decode./param>
         /// <returns>The decoded URL</returns>
-        public static string UrlDecode(string url)
+        public static string UrlDecode(string str)
         {
-            if ((url == null) || (url == string.Empty))
+            if (string.IsNullOrEmpty(str))
             {
                 return string.Empty;
             }
 
-            var data = Encoding.UTF8.GetBytes(url);
+            var data = Encoding.UTF8.GetBytes(str);
+
             return new string(Encoding.UTF8.GetChars(UrlDecodeToBytes(data, 0, data.Length)));
         }
 
         /// <summary>
-        /// Decode an URL using the specified encoding
+        /// Converts a URL-encoded string into a decoded string, using the specified encoding object.
         /// </summary>
-        /// <param name="str">The string to encode</param>
-        /// <param name="e">The Encoding object that specifies the encoding scheme</param>
-        /// <returns>The encoded string</returns>
+        /// <param name="str">The string to decode.</param>
+        /// <param name="e">The <see cref="Encoding"/> that specifies the decoding scheme.</param>
+        /// <returns>A decoded string.</returns>
         public static string UrlDecode(string str, Encoding e)
         {
             if ((str == null) || (str == string.Empty))
@@ -206,49 +231,67 @@ namespace System.Web
         }
 
         /// <summary>
-        /// Decode an encoded URL
+        /// Converts a URL-encoded byte array into a decoded string using the specified encoding object, starting at the specified position in the array, and continuing for the specified number of bytes.
         /// </summary>
-        /// <param name="bytes">The array of bytes to decode</param>
+        /// <param name="bytes">The array of bytes to decode.</param>
         /// <param name="offset">The position in the byte to begin decoding</param>
-        /// <param name="count">The number of bytes to decode</param>
-        /// <param name="e">The Encoding object that specifies the encoding scheme</param>
-        /// <returns>The decoded URL</returns>
-        public static string UrlDecode(byte[] bytes, int offset, int count, System.Text.Encoding e) => new string(e.GetChars(UrlDecodeToBytes(bytes, 0, bytes.Length)));
+        /// <param name="count">The number of bytes to decode.</param>
+        /// <param name="e">The <see cref="Encoding"/> object that specifies the encoding scheme.</param>
+        /// <returns>A decoded string.</returns>
+        public static string UrlDecode(
+            byte[] bytes,
+            int offset,
+            int count,
+            Encoding e) => new(e.GetChars(UrlDecodeToBytes(
+                bytes,
+                offset,
+                count)));
 
         /// <summary>
-        /// Decode an encoded URL
+        /// Converts a URL-encoded byte array into a decoded string using the specified decoding object.
         /// </summary>
-        /// <param name="bytes">The array of bytes to decode</param>
-        /// <param name="e">The Encoding object that specifies the encoding scheme</param>
-        /// <returns>The decoded URL</returns>
-        public static string UrlDecode(byte[] bytes, System.Text.Encoding e) => new string(e.GetChars(UrlDecodeToBytes(bytes, 0, bytes.Length)));
+        /// <param name="bytes">The array of bytes to decode.</param>
+        /// <param name="e">The <see cref="Encoding"/> object that specifies the encoding scheme.</param>
+        /// <returns>A decoded string.</returns>
+        public static string UrlDecode(
+            byte[] bytes,
+            Encoding e) => new string(e.GetChars(UrlDecodeToBytes(
+                bytes,
+                0,
+                bytes.Length)));
 
         /// <summary>
-        /// Decode bytes array to byte array
+        /// Converts a URL-encoded array of bytes into a decoded array of bytes, starting at the specified position in the array and continuing for the specified number of bytes.
         /// </summary>
-        /// <param name="bytes">The array of bytes to decode</param>
-        /// <param name="offset">The position in the byte array at which to begin decoding</param>
-        /// <param name="count">The number of bytes to decode</param>
-        /// <returns></returns>
-        public static byte[] UrlDecodeToBytes(byte[] bytes, int offset, int count)
+        /// <param name="bytes">The array of bytes to decode.</param>
+        /// <param name="offset">The position in the byte array at which to begin decoding.</param>
+        /// <param name="count">The number of bytes to decode.</param>
+        /// <returns>A decoded array of bytes.</returns>
+        public static byte[] UrlDecodeToBytes(
+            byte[] bytes,
+            int offset,
+            int count)
         {
             var length = 0;
             var sourceArray = new byte[count];
+
             for (var i = 0; i < count; i++)
             {
                 var index = offset + i;
                 var num4 = bytes[index];
+
                 if (num4 == 0x2b)
                 {
                     num4 = 0x20;
                 }
-                else if ((num4 == 0x25) &&
-                         (i < (count - 2)))
+                else if ((num4 == 0x25)
+                         && (i < (count - 2)))
                 {
                     var num5 = HexToInt((char)bytes[index + 1]);
                     var num6 = HexToInt((char)bytes[index + 2]);
-                    if ((num5 >= 0) &&
-                        (num6 >= 0))
+
+                    if ((num5 >= 0)
+                        && (num6 >= 0))
                     {
                         num4 = (byte)((num5 << 4) | num6);
                         i += 2;
@@ -256,46 +299,55 @@ namespace System.Web
                 }
                 sourceArray[length++] = num4;
             }
+
             if (length < sourceArray.Length)
             {
                 var destinationArray = new byte[length];
                 Array.Copy(sourceArray, destinationArray, length);
                 sourceArray = destinationArray;
             }
+
             return sourceArray;
         }
 
         /// <summary>
-        /// Decode bytes array to byte array
+        /// Converts a URL-encoded string into a decoded array of bytes using the specified decoding object.
         /// </summary>
-        /// <param name="str">The string to encode</param>
-        /// <param name="e">The Encoding object that specifies the encoding scheme</param>
-        /// <returns></returns>
-        public static byte[] UrlDecodeToBytes(string str, System.Text.Encoding e)
+        /// <param name="str">The string to encode.</param>
+        /// <param name="e">The <see cref="Encoding"/> object that specifies the encoding scheme.</param>
+        /// <returns>A decoded array of bytes.</returns>
+        public static byte[] UrlDecodeToBytes(
+            string str,
+            Encoding e)
         {
             var data = e.GetBytes(str);
             return UrlDecodeToBytes(data, 0, data.Length);
         }
 
         /// <summary>
-        /// Decode bytes array to byte array in UTF8
+        /// Converts a URL-encoded string into a decoded array of bytes.
         /// </summary>
-        /// <param name="str">The string to encode</param>
-        /// <returns></returns>
-        public static byte[] UrlDecodeToBytes(string str) => UrlDecodeToBytes(str, Encoding.UTF8);
+        /// <param name="str">The string to decode.</param>
+        /// <returns>A decoded array of bytes.</returns>
+        public static byte[] UrlDecodeToBytes(string str) => UrlDecodeToBytes(
+            str,
+            Encoding.UTF8);
 
         /// <summary>
-        /// Decode bytes array to byte array
+        /// Converts a URL-encoded array of bytes into a decoded array of bytes.
         /// </summary>
-        /// <param name="bytes">The array of bytes to decode</param>
-        /// <returns></returns>
-        public static byte[] UrlDecodeToBytes(byte[] bytes) => UrlDecodeToBytes(bytes, 0, bytes.Length);
+        /// <param name="bytes">The array of bytes to decode.</param>
+        /// <returns>A decoded array of bytes.</returns>
+        public static byte[] UrlDecodeToBytes(byte[] bytes) => UrlDecodeToBytes(
+            bytes,
+            0,
+            bytes.Length);
 
         /// <summary>
-        /// Get the int value of a char
+        /// Get the int value of a char.
         /// </summary>
-        /// <param name="h">a char</param>
-        /// <returns>The int value of the char</returns>
+        /// <param name="h">The <see cref="char"/> to convert.</param>
+        /// <returns>The <see cref="int"/> value of the <see cref="char"/>.</returns>
         public static int HexToInt(char h)
         {
             if ((h >= '0') &&
@@ -303,16 +355,19 @@ namespace System.Web
             {
                 return (h - '0');
             }
+
             if ((h >= 'a') &&
                 (h <= 'f'))
             {
                 return ((h - 'a') + 10);
             }
+
             if ((h >= 'A') &&
                 (h <= 'F'))
             {
                 return ((h - 'A') + 10);
             }
+
             return -1;
         }
     }
