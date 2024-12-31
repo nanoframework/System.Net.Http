43943a412c12f9fcb17da90d6fb2295e5304d5a6
mizady <90141999+mizady@users.noreply.github.com>
2024-12-31 16:24:18 -0500

    Update ByteArrayContentTest
    Updated deprecated assert functions.
    
    Re-enable commented out tests that are now passing.
    
    Add Skip to a test due to read-only memory stream not yet being supported.

--- Tests/HttpUnitTests/ByteArrayContentTest.cs
+++ Tests/HttpUnitTests/ByteArrayContentTest.cs
@@ -8,7 +8,6 @@ using nanoFramework.TestFramework;
 using System;
 using System.IO;
 using System.Net.Http;
-using System.Text;
 
 namespace HttpUnitTests
 {
@@ -18,113 +17,106 @@ namespace HttpUnitTests
         [TestMethod]
         public void Ctor_NullSourceArray_ThrowsArgumentNullException()
         {
-            Assert.Throws(typeof(ArgumentNullException), () => new ByteArrayContent(null));
+            Assert.ThrowsException(typeof(ArgumentNullException), () => new ByteArrayContent(null));
         }
 
         [TestMethod]
         public void Ctor_NullSourceArrayWithRange_ThrowsArgumentNullException()
         {
-            Assert.Throws(typeof(ArgumentNullException), () => new ByteArrayContent(null, 0, 1));
+            Assert.ThrowsException(typeof(ArgumentNullException), () => new ByteArrayContent(null, 0, 1));
         }
 
         [TestMethod]
         public void Ctor_EmptySourceArrayWithRange_ThrowsArgumentOutOfRangeException()
         {
-            Assert.Throws(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[0], 0, 1));
+            Assert.ThrowsException(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[0], 0, 1));
         }
 
         [TestMethod]
         public void Ctor_StartIndexTooBig_ThrowsArgumentOufOfRangeException()
         {
-            Assert.Throws(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], 5, 1));
+            Assert.ThrowsException(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], 5, 1));
         }
 
         [TestMethod]
         public void Ctor_StartIndexNegative_ThrowsArgumentOutOfRangeException()
         {
-            Assert.Throws(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], -1, 1));
+            Assert.ThrowsException(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], -1, 1));
         }
 
         [TestMethod]
         public void Ctor_LengthTooBig_ThrowsArgumentOutOfRangeException()
         {
-            Assert.Throws(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], 1, 5));
+            Assert.ThrowsException(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], 1, 5));
         }
 
         [TestMethod]
         public void Ctor_LengthPlusOffsetCauseIntOverflow_ThrowsArgumentOutOfRangeException()
         {
-            Assert.Throws(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], 1, int.MaxValue));
+            Assert.ThrowsException(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], 1, int.MaxValue));
         }
 
         [TestMethod]
         public void Ctor_LengthNegative_ThrowsArgumentOutOfRangeException()
         {
-            Assert.Throws(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], 0, -1));
+            Assert.ThrowsException(typeof(ArgumentOutOfRangeException), () => new ByteArrayContent(new byte[5], 0, -1));
         }
 
-        // TODO need to fix processing of exception
-        //[TestMethod]
-        //public void ContentLength_UseWholeSourceArray_LengthMatchesArrayLength()
-        //{
-        //    var contentData = new byte[10];
-        //    var content = new ByteArrayContent(contentData);
+        [TestMethod]
+        public void ContentLength_UseWholeSourceArray_LengthMatchesArrayLength()
+        {
+            var contentData = new byte[10];
+            var content = new ByteArrayContent(contentData);
 
-        //    Assert.Equal(contentData.Length, content.Headers.ContentLength);
-        //}
+            Assert.AreEqual(contentData.Length, content.Headers.ContentLength);
+        }
 
-        // TODO need to fix processing of exception
-        //[TestMethod]
-        //public void ContentLength_UsePartialSourceArray_LengthMatchesArrayLength()
-        //{
-        //    Assert.SkipTest("Test disabled on API failure");
+        [TestMethod]
+        public void ContentLength_UsePartialSourceArray_LengthMatchesArrayLength()
+        {
+            var contentData = new byte[10];
+            var content = new ByteArrayContent(contentData, 5, 3);
 
-        //    // TODO need to fix edge case in ByteArrayContent
-
-        //    var contentData = new byte[10];
-        //    var content = new ByteArrayContent(contentData, 5, 3);
-
-        //    Assert.Equal(3, content.Headers.ContentLength);
-        //}
+            Assert.AreEqual(3, content.Headers.ContentLength);
+        }
 
         [TestMethod]
         public void ReadAsStreamAsync_EmptySourceArray_Succeed()
         {
             var content = new ByteArrayContent(new byte[0]);
             Stream stream = content.ReadAsStream();
-            Assert.Equal(0, stream.Length);
+            Assert.AreEqual(0, stream.Length);
         }
 
-        // TODO need to fix processing of exception
-        //[TestMethod]
-        //public void ReadAsStream_Call_MemoryStreamWrappingByteArrayReturned()
-        //{
-        //    Assert.SkipTest("Test disabled on API failure");
+        [TestMethod]
+        public void ReadAsStream_Call_MemoryStreamWrappingByteArrayReturned()
+        {
+            Assert.SkipTest("Read only MemoryStream not implemented yet");
 
-        //    // TODO need to fix edge case in stream reader
+            // TODO need to fix edge case in stream reader
+            // (stream reader does not have read only stream properly implemented)
 
-        //    var contentData = new byte[10];
-        //    var content = new MockByteArrayContent(contentData, 5, 3);
+            var contentData = new byte[10];
+            var content = new MockByteArrayContent(contentData, 5, 3);
 
-        //    Stream stream = content.ReadAsStream();
-        //    Assert.False(stream.CanWrite);
-        //    Assert.Equal(3, stream.Length);
-        //    Assert.Equal(0, content.CopyToCount);
-        //}
+            Stream stream = content.ReadAsStream();
+            Assert.IsFalse(stream.CanWrite);
+            Assert.AreEqual(3, stream.Length);
+            Assert.AreEqual(0, content.CopyToCount);
+        }
 
-        // TODO need to fix processing of exception
-        //[TestMethod]
-        //public void CopyTo_NullDestination_ThrowsArgumentNullException()
-        //{
-        //    byte[] contentData = CreateSourceArray();
-        //    var content = new ByteArrayContent(contentData);
+        [TestMethod]
+        public void CopyTo_NullDestination_ThrowsArgumentNullException()
+        {
+            byte[] contentData = CreateSourceArray();
+            var content = new ByteArrayContent(contentData);
 
-        //    Assert.Throws(typeof(ArgumentNullException),
-        //        () =>
-        //        {
-        //            content.CopyTo(null);
-        //        });
-        //}
+            Assert.ThrowsException(typeof(ArgumentNullException),
+                () =>
+                {
+                    content.CopyTo(null);
+                });
+        }
 
         [TestMethod]
         public void CopyTo_UseWholeSourceArray_WholeContentCopied()
@@ -135,7 +127,7 @@ namespace HttpUnitTests
             var destination = new MemoryStream();
             content.CopyTo(destination);
 
-            Assert.Equal(contentData.Length, destination.Length);
+            Assert.AreEqual(contentData.Length, destination.Length);
             CheckResult(destination, 0);
         }
 
@@ -148,22 +140,21 @@ namespace HttpUnitTests
             var destination = new MemoryStream();
             content.CopyTo(destination);
 
-            Assert.Equal(5, destination.Length);
+            Assert.AreEqual(5, destination.Length);
             CheckResult(destination, 3);
         }
 
-        // TODO need to fix processing of exception
-        //[TestMethod]
-        //public void CopyTo_UseEmptySourceArray_NothingCopied()
-        //{
-        //    var contentData = new byte[0];
-        //    var content = new ByteArrayContent(contentData, 0, 0);
+        [TestMethod]
+        public void CopyTo_UseEmptySourceArray_NothingCopied()
+        {
+            var contentData = new byte[0];
+            var content = new ByteArrayContent(contentData, 0, 0);
 
-        //    var destination = new MemoryStream();
-        //    content.CopyTo(destination);
+            var destination = new MemoryStream();
+            content.CopyTo(destination);
 
-        //    Assert.Equal(0, destination.Length);
-        //}
+            Assert.AreEqual(0, destination.Length);
+        }
 
         #region Helper methods
 
@@ -183,12 +174,12 @@ namespace HttpUnitTests
             var destinationData = new byte[destination.Length];
             int read = destination.Read(destinationData, 0, destinationData.Length);
 
-            Assert.Equal(destinationData.Length, read);
-            Assert.Equal(firstValue, destinationData[0]);
+            Assert.AreEqual(destinationData.Length, read);
+            Assert.AreEqual(firstValue, destinationData[0]);
 
             for (int i = 1; i < read; i++)
             {
-                Assert.True((destinationData[i] == (destinationData[i - 1] + 1)) ||
+                Assert.IsTrue((destinationData[i] == (destinationData[i - 1] + 1)) ||
                     ((destinationData[i] == 0) && (destinationData[i - 1] != 0)));
             }
         }
