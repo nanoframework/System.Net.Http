f6dcce0b078895af2698c80a61b105ab8529e44c
benyuz <305378604@qq.com>
2026-05-04 21:34:15 +0800

    Add unit tests for WebHeaderCollection Authorization headers
    This test class verifies the behavior of adding various Authorization headers to a WebHeaderCollection, ensuring that no exceptions are thrown for valid inputs and that the values are stored correctly.

---
+++ Tests/HttpUnitTests/WebHeaderCollectionTests.cs
@@ -0,0 +1,55 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using nanoFramework.TestFramework;
+using System.Net;
+
+namespace HttpUnitTests
+{
+    [TestClass]
+    public class WebHeaderCollectionTests
+    {
+        [TestMethod]
+        public void Add_Authorization_BearerWithSpaceAndNoValue_ShouldNotThrow()
+        {
+            var headers = new WebHeaderCollection();
+            headers.Add("Authorization: Bearer ");
+        }
+
+        [TestMethod]
+        public void Add_Authorization_NoSpaceSingleChar_ShouldNotThrow()
+        {
+            var headers = new WebHeaderCollection();
+            headers.Add("Authorization: 1");
+        }
+
+        [TestMethod]
+        public void Add_Authorization_ValidBearer_ShouldSucceed()
+        {
+            var headers = new WebHeaderCollection();
+            headers.Add("Authorization: Bearer a11111");
+            string value = headers["Authorization"];
+            Assert.AreEqual("Bearer a11111", value);
+        }
+
+        [TestMethod]
+        public void Add_Authorization_ValidTestValue_ShouldSucceed()
+        {
+            var headers = new WebHeaderCollection();
+            headers.Add("Authorization: test 1");
+            string value = headers["Authorization"];
+            Assert.AreEqual("test 1", value);
+        }
+
+        [TestMethod]
+        public void Add_Authorization_ValidSingleLetterPair_ShouldSucceed()
+        {
+            var headers = new WebHeaderCollection();
+            headers.Add("Authorization: a b");
+            string value = headers["Authorization"];
+            Assert.AreEqual("a b", value);
+        }
+    }
+}
