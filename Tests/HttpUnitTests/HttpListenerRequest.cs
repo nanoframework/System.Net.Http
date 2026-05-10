de2844edd840c6f49af7bee4d203149fb8a9ed89
yu ben <benyu23@qq.com>
2026-05-10 19:41:34 +0800

    Fix ArgumentOutOfRangeException when validating Basic Authorization header

---
+++ Tests/HttpUnitTests/HttpListenerRequest.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Net;
+using nanoFramework.TestFramework;
+
+namespace HttpUnitTests
+{
+    internal class HttpListenerRequestTests
+    {
+        // Verifies that malformed Authorization header (no space) does not cause a crash
+        [TestMethod]
+        public void Add_Authorization_NoSpaceMultipleChars_ShouldNotThrow()
+        {
+            var headers = new WebHeaderCollection();
+            headers.Add("Authorization: a111111");
+            string value = headers["Authorization"];
+            Assert.AreEqual("a111111", value);
+        }
+
+        // Verifies that a properly formatted Authorization header (with space) is parsed and stored correctly
+        [TestMethod]
+        public void Add_Authorization_ValidBasicToken_ShouldSucceed()
+        {
+            var headers = new WebHeaderCollection();
+            headers.Add("Authorization: Basic a111111");
+            string value = headers["Authorization"];
+            Assert.AreEqual("Basic a111111", value);
+        }
+    }
+}
